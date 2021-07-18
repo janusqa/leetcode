d42030dac73177bd7eba6eca9e6a8ce2e7c25198
Janus Developer <dev@retrievo.net>
2021-07-17 21:18:51 -0400

    maximum depth binary tree

--- _maxdepthbinarytree/main.py
+++ _maxdepthbinarytree/main.py
@@ -12,6 +12,34 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        max_depth = 0
+        queue = []
+        if root:
+            queue.append({"node": root, "depth": 1})
+        while len(queue) > 0:
+            curr_ptr = queue.pop(0)
+            if curr_ptr["node"]:
+                max_depth = max(max_depth, curr_ptr["depth"])
+                if curr_ptr["node"].left:
+                    queue.append(
+                        {"node": curr_ptr["node"].left, "depth": curr_ptr["depth"] + 1}
+                    )
+                if curr_ptr["node"].right:
+                    queue.append(
+                        {"node": curr_ptr["node"].right, "depth": curr_ptr["depth"] + 1}
+                    )
+
+        return max_depth
 
 
-print(Tree().build([3, 9, 20, None, None, 15, 7]).to_list())
+print(Solution().maxDepth(Tree().build([3, 9, 20, None, None, 15, 7]).root))
+print(Solution().maxDepth(Tree().build([1, None, 2]).root))
+print(Solution().maxDepth(Tree().build([]).root))
+print(Solution().maxDepth(Tree().build([0]).root))
+
+
+##Test tree
+# print(Tree().build([3, 9, 20, None, None, 15, 7]).to_list())
+# print(Tree().build([1, None, 2]).to_list())
+# print(Tree().build([]).to_list())
+# print(Tree().build([0]).to_list())
